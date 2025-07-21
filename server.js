8b2d700525330b27b12aaad431d5fb47a4c6c84a
mgcpixxa12 <johnpaulcheramie@gmail.com>
2025-07-20 20:56:48 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,38 +5,26 @@ const io = require("socket.io")(http, {
   cors: { origin: "*" }
 });
 
-const PORT = process.env.PORT || 3000;
-app.set('port', PORT);
-
 const players = {};
 
 io.on("connection", (socket) => {
   console.log("✅ Player connected:", socket.id);
-  players[socket.id] = { x: 0, y: 0 };
+  players[socket.id] = { x: 0, y: 0, d: 2 };
 
   socket.emit("init", players);
-
-  socket.broadcast.emit("player-joined", {
-    id: socket.id,
-    x: players[socket.id].x,
-    y: players[socket.id].y,
-  });
+  socket.broadcast.emit("player-joined", { id: socket.id, ...players[socket.id] });
 
   socket.on("move", (pos) => {
     if (players[socket.id]) {
       players[socket.id] = pos;
-      socket.broadcast.emit("player-move", {
-        id: socket.id,
-        x: pos.x,
-        y: pos.y,
-      });
+      socket.broadcast.emit("player-move", { id: socket.id, ...pos });
     }
   });
 
   socket.on("disconnect", () => {
     console.log("❌ Player disconnected:", socket.id);
     delete players[socket.id];
-    socket.broadcast.emit("player-left", socket.id);
+    io.emit("player-left", socket.id);
   });
 });
 
@@ -44,6 +32,6 @@ app.get("/", (req, res) => {
   res.send("Multiplayer server running.");
 });
 
-http.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+http.listen(process.env.PORT || 3000, () => {
+  console.log("🚀 Server is running");
 });
